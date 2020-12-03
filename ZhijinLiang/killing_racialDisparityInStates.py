c89dc7a028db5cc88f8e14dc91ce251218646597
zhijin <JasminL_work@163.com>
2020-12-02 20:35:14 -0800

    Add killing over population and create demo

--- ZhijinLiang/killing_racialDisparityInStates.py
+++ ZhijinLiang/killing_racialDisparityInStates.py
@@ -1,14 +1,16 @@
 # In this file, I write two pie charts to show racial disparities in a few selected states,
 # including percentage of population and percentage of police killings
 # The dataset used in this file is '2013-2019 Killings by State.csv'
-
 import pandas as pd
 import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-
 
 def racial_disparity_states_population(fileName, outputName = 'racial_disparity_states_population', statesList = ['California', 'New York', 'Texas']):
+    """
+    This function is used to generate the percentage of population for different races in given states
+    :param fileName: .csv file contains the population and killing number in each state
+    :param outputName: A given file in csv format, it should contains a 'State' column
+    :param statesList: a list of interested states
+    """
     data, populationInfo_pd, _ = extractInfo(fileName, statesList)
     fig = px.sunburst(populationInfo_pd, path = ['State', 'race'], values = 'population',
                       title = 'Percentage of Population')
@@ -17,6 +19,12 @@ def racial_disparity_states_population(fileName, outputName = 'racial_disparity_
     fig.show()
 
 def racial_disparity_states_killed(fileName, outputName = 'racial_disparity_states_killed', statesList = ['California', 'New York', 'Texas']):
+    """
+    This function is used to generate the percentage of the number of killing for different races in given states
+    :param fileName: .csv file contains the population and killing number in each state
+    :param outputName: A given file in csv format, it should contains a 'State' column
+    :param statesList: a list of interested states
+    """
     data, _, killedInfo_pd = extractInfo(fileName, statesList)
     fig = px.sunburst(killedInfo_pd, path=['State', 'race'], values='people_killed',
                       title='Percentage of police killings')
@@ -24,6 +32,20 @@ def racial_disparity_states_killed(fileName, outputName = 'racial_disparity_stat
     fig.write_html(outputName+'.html')  # store in html format
     fig.show()
 
+def racial_disparity_states_killedOverPopulation(fileName, outputName = 'racial_disparity_states_killed1m', statesList = ['California', 'New York', 'Texas']):
+    """
+    This function is used to generate the percentage of the number of killing for different races in given states(killing over population)
+    :param fileName: .csv file contains the population and killing number in each state
+    :param outputName: A given file in csv format, it should contains a 'State' column
+    :param statesList: a list of interested states
+    """
+    data, populationInfo , killedInfo_pd = extractInfo(fileName, statesList)
+    killedOverPopulation_pd = integratePopulationAndKilling(populationInfo, killedInfo_pd, scale = 1e6) # the scale in million
+    fig = px.sunburst(killedOverPopulation_pd, path=['State', 'race'], values='killedOverPopulation',
+                      title='Percentage of police killings(killing over every 1 million population)')
+
+    fig.write_html(outputName + '.html')  # store in html format
+    fig.show()
 
 def extractInfo(fileName, statesList):
     """
@@ -44,6 +66,35 @@ def extractInfo(fileName, statesList):
     killedInfo = processsKilledInfo(data, statesList)
 
     return data, populationInfo, killedInfo
+
+def integratePopulationAndKilling(populationInfo, killedInfo, scale):
+    """
+    This function is used generate a dataframe in format: State|race|killedOverPopulation = \frac{killing}{Population/scale}|
+    :param populationInfo: population info, format is State|race|population|
+    :param killedInfo: the number of killing information, format is 'State|race|people_killed'
+    :param scale: for instance, 1 million
+    :return: a pd.DataFrame in a given format
+    """
+    # store the data as .csv
+    # populationInfo.to_csv('population.csv', index=False)
+    # killedInfo.to_csv('killed.csv', index=False)
+
+    # re-format the dataset as : State|race|\frac{killing}{Population}|
+    killedOverPopulationInfo = killedInfo.copy()
+    # drop the data entry whose race is unkown (data cleaning)
+    killedOverPopulationInfo = killedOverPopulationInfo[killedOverPopulationInfo.race != 'Unknown']
+    killedOverPopulation_in_pd = []
+    for (index, row) in killedOverPopulationInfo.iterrows():
+        # find population in populationInfo
+        tmp = populationInfo.loc[populationInfo['State'] == row['State']]
+        population = tmp.loc[populationInfo['race'] == row['race']]['population'].values.item()
+
+        # compute people_killed/population
+        killedOverPopulation_in_pd.append(row['people_killed']/(population/scale))
+
+    killedOverPopulationInfo['people_killed'] = killedOverPopulation_in_pd
+    killedOverPopulationInfo = killedOverPopulationInfo.rename(columns={'people_killed': 'killedOverPopulation'})
+    return killedOverPopulationInfo
 
 def processPopulationInfo(data, statesList):
     """
@@ -87,7 +138,7 @@ def processsKilledInfo(data, statesList):
     """
     raceList = ['Black', 'Hispanic', 'Native American', 'Asian', 'Pacific Islander', 'White', 'Unknown']
 
-    # Re-format the dataset as : State|race|population|
+    # Re-format the dataset as : State|race|people_killed|
     killedInfo = pd.DataFrame()
     raceList_inPD = []
     stateList_inPD = []
@@ -119,7 +170,6 @@ def processsKilledInfo(data, statesList):
 
 if __name__ == '__main__':
     fileName = '2013-2019 Killings by State.csv'
-    racial_disparity_states_population(fileName)
-    racial_disparity_states_killed(fileName)
-
-
+    # racial_disparity_states_population(fileName)
+    # racial_disparity_states_killed(fileName)
+    racial_disparity_states_killedOverPopulation(fileName)
